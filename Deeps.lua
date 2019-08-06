5c458d9541db140259253f139f626952b8503225
branco-medeiros <branco-medeiros@users.noreply.github.com>
2019-08-06 07:22:39 -0300

    zipzipzip

--- Deeps.lua
+++ Deeps.lua
@@ -6,6 +6,7 @@ local Deeps = LibStub("AceAddon-3.0"):NewAddon(
 	"AceEvent-3.0"
 )
 local Config = LibStub("AceConfig-3.0")
+local AceGUI = LibStub("AceGUI-3.0")
 
 local options = {
 	name="Deeps",
@@ -58,3 +59,55 @@ end
 function Deeps:setDebugging(value)
 	self.debugging = value
 end
+
+
+function Deeps:initializeUI()
+	local frame = AceGUI:Create("Window")
+	frame:SetCallback("OnClose", function(widget) AceGUI:Release(widget) end)
+	self.MainFrame = frame
+	frame:SetLayout("Flow")
+	
+	local LeftFrame = AceGUI:Create("SimpleGroup")
+	LeftFrame:SetRelativeWidth(0.5)
+	LeftFrame:SetFullHeight(true)
+	frame:AddChild(LeftFrame)
+	
+	local RightFrame = AceGUI:Create("SimpleGroup")
+	RightFrame:SetRelativeWidth(0.5)
+	RightFrame:SetFullHeight(true)
+	frame:AddChild(RightFrame)
+	
+	LeftFrame:SetLayout("List")
+	
+	local w = AceGUI:Create("Label")
+	w:SetText("Spec")
+	w:SetFullWidth(true)
+	LeftFrame:AddChild(w)
+	
+	w = AceGUI:Create("DropdownGroup")
+	w:SetFullWidth(true)
+	LeftFrame:AddChild(w)
+	self.SpecList = w
+	
+	w = AceGUI:Create("Label")
+	w:SetText("Priority List")
+	w:SetFullWidth(true)
+	LeftFrame:AddChild(w)
+	
+	local container = AceGUI:Create("SimpleGroup")
+	container:SetFullWidth(true)
+	container:SetLayout("Fill")
+	container:SetHeight(100)
+  LefFrame:AddChild(container)
+
+	w = AceGUI:Create("ScrollFrame")
+	w:SetLayout("List")
+	container:AddChild(w)
+	self.PriorityList = w
+	
+	
+	
+	
+	
+end
+
